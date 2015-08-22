0cffdd3e1a71df453b42f95afe8650452d49dfe1
Jerrod Junker <unsafepond@gmail.com>
2015-08-22 17:57:28 -0600

    finished hints. time for refactoring

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,5 +2,15 @@ require 'sinatra'
 require "sinatra/reloader"
 number = rand(100)
 get '/' do
-	erb :index, :locals => {:number => number}
+	guess = params["guess"].to_i
+	if guess > (number+5)
+		message = "Way Too High!!\n\n"
+	elsif guess < (number-5)
+		message = "Way Too Low!!\n\n"
+	elsif guess > number
+		message = "Too High!\n\n"
+	elsif guess < number
+		message = "Too Low!\n\n"
+	end
+	erb :index, :locals => {:number => number, :message => message}
 end
